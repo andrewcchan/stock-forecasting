c3b50de10785702b38f45a3ca5e2bccfb1eb3265
andrewcchan <40149441+andrewcchan@users.noreply.github.com>
2021-07-23 22:36:17 -0700

    MOD wireframe

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.title('Stock Forecasting')
+
+st.write('## Select Ticker Symbol')
+
+st.write('## Select Forecasting Algo')
+
+st.write('## Tune Hyperparameters')
+
+st.write('## Graph')
+
