c716dfa2b1e2e3da0f60fb9458f941f1e8bfe6f8
andrewcchan <40149441+andrewcchan@users.noreply.github.com>
2021-07-23 22:47:28 -0700

    MOD: add time series plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,9 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+keras = tf.keras
 
 st.title('Stock Forecasting')
 
@@ -14,3 +17,44 @@ st.write('## Tune Hyperparameters')
 
 st.write('## Graph')
 
+def plot_series(time, series, format="-", start=0, end=None, label=None):
+    plt.plot(time[start:end], series[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    if label:
+        plt.legend(fontsize=14)
+    plt.grid(True)
+    
+def trend(time, slope=0):
+    return slope * time
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+def white_noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+time = np.arange(4 * 365 + 1)
+
+slope = 0.05
+baseline = 10
+amplitude = 40
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+
+noise_level = 5
+noise = white_noise(time, noise_level, seed=42)
+
+series += noise
+
+plt.figure(figsize=(10, 6))
+plot_series(time, series)
+plt.show()
