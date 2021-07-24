a008d8b1ae7ee1568b7ee248e61806f32eb65ded
andrewcchan <40149441+andrewcchan@users.noreply.github.com>
2021-07-24 10:43:47 -0700

    MOD: split into multiple py

---
+++ base_functions.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# functions
+def plot_series(time, series, tic_symbol, format="-", start=0, end=None, label=None):
+    fig = plt.figure(figsize=(10, 6))
+    plt.plot(time[start:end], series[start:end], format, label=label)
+    plt.xlabel("Date")
+    plt.ylabel("Closing Price")
+    title = tic_symbol + ' 1 Year Closing Price'
+    plt.title(title)
+    plt.grid(True)
+    st.pyplot(fig)
+    
+def trend(time, slope=0):
+    return slope * time
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+def white_noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from gsheetsdb import connect
-
-
+import base_functions.plot_series as plot_series
 
 # APP
 
@@ -27,36 +26,6 @@ st.write('## Tune Hyperparameters')
 
 st.write('## Graph')
 
-def plot_series(time, series, tic_symbol, format="-", start=0, end=None, label=None):
-    fig = plt.figure(figsize=(10, 6))
-    plt.plot(time[start:end], series[start:end], format, label=label)
-    plt.xlabel("Date")
-    plt.ylabel("Closing Price")
-    title = tic_symbol + '1 Year Closing Price'
-    plt.title(title)
-    plt.grid(True)
-    st.pyplot(fig)
-    
-def trend(time, slope=0):
-    return slope * time
-
-def seasonal_pattern(season_time):
-    """Just an arbitrary pattern, you can change it if you wish"""
-    return np.where(season_time < 0.4,
-                    np.cos(season_time * 2 * np.pi),
-                    1 / np.exp(3 * season_time))
-
-def seasonality(time, period, amplitude=1, phase=0):
-    """Repeats the same pattern at each period"""
-    season_time = ((time + phase) % period) / period
-    return amplitude * seasonal_pattern(season_time)
-
-def white_noise(time, noise_level=1, seed=None):
-    rnd = np.random.RandomState(seed)
-    return rnd.randn(len(time)) * noise_level
-
-
-
 
 # LOAD DATA
 # Create a connection object.
@@ -69,7 +38,7 @@ def run_query(query):
     rows = conn.execute(query, headers=1)
     return rows
 
-sheet_url = st.secrets["gsheets_url"]
+sheet_url = st.secrets["gsheets_url_goog"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 dates = []
