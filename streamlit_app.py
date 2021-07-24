7d11327433932bc07a6b3d827317a25d8d06e5df
andrewcchan <40149441+andrewcchan@users.noreply.github.com>
2021-07-24 07:36:34 -0700

    MOD: add example data request

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
+from gsheetsdb import connect
+
+# LOAD DATA
+# Create a connection object.
+conn = connect()
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    return rows
+
+sheet_url = st.secrets["gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+# Print results.
+for row in rows:
+    st.write(f"Date: {row.Date}, Close:{row.Close}:")
+# APP
+
 keras = tf.keras
 
 st.title('Stock Forecasting')
